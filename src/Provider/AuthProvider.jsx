e033c4a387db5c4ae88956d5a5a40c3917866f71
sharifulislamudoy <surifroton301@gmail.com>
2025-05-19 21:45:03 +0600

    start the project and install react-vite,tailwind css,daisy ui, react router, firebase and setup .env.local file for firebse and setup context for user and set up initial route setup

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,15 @@
+import React, { createContext } from 'react';
+
+
+
+export const authContext = createContext();
+
+const AuthProvider = () => {
+
+    
+
+
+    return <authContext></authContext>
+};
+
+export default AuthProvider;
